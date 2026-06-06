0ed0e6f609d780e18a21cea06618de72b4dbfff7
MD SADMAN <sadmansaif522@gmail.com>
2026-06-06 09:40:19 +0600

    Create Even or Odd Checker.cs

---
+++ Even or Odd Checker.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num = 6;
+
+        if (num % 2 == 0)
+        {
+            Console.WriteLine("Even Number");
+        }
+        else
+        {
+            Console.WriteLine("Odd Number");
+        }
+    }
+}
