d49a38f25a331c7ab120eee58fddeea6cc645416
MD SADMAN <sadmansaif522@gmail.com>
2026-06-06 08:01:40 +0600

    Create variable.cs

---
+++ variable.cs
@@ -0,0 +1,14 @@
+using System;
+
+class program
+{
+    static void Main()
+    {
+        string name = "sadman islam";
+        console.WriteLine(name);
+        console.WriteLine("my name is " + name);
+        console.WriteLine($"my name is {name}");
+
+        
+    }
+}
