f39bbeae2f229ade3845c254f064546183942948
MD SADMAN <sadmansaif522@gmail.com>
2026-06-06 08:26:15 +0600

    Create variable_02.cs

---
+++ variable_02.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Sadman";
+        string batch = "59A";
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Batch: " + batch);
+    }
+}
