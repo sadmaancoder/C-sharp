4045ec7e14ebaee56b035202fd042b78e6f8918a
MD SADMAN <sadmansaif522@gmail.com>
2026-06-06 20:35:01 +0600

    Create Countdown (While Loop).cs

---
+++ Countdown (While Loop).cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int count = 5;
+
+        while (count > 0)
+        {
+            Console.WriteLine(count);
+            count--;
+        }
+
+        Console.WriteLine("Happy New Year!");
+    }
+}
