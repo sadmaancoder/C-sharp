7cf1cd7b62377ac7a5a7f4c935d3e9761cb45848
MD SADMAN <sadmansaif522@gmail.com>
2026-06-06 09:34:10 +0600

    Create string.cs

---
+++ string.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string txt = "Hello World";
+
+        Console.WriteLine(txt);
+        Console.WriteLine(txt.Length);
+        Console.WriteLine(txt.ToUpper());
+        Console.WriteLine(txt.ToLower());
+    }
+}
