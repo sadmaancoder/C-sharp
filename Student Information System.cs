16488ca477813b2488050d94ca68d6314a895049
MD SADMAN <sadmansaif522@gmail.com>
2026-06-06 09:37:39 +0600

    Create Student Information System.cs

---
+++ Student Information System.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Sadman";
+        int age = 20;
+        double cgpa = 3.75;
+        char grade = 'A';
+        bool isStudent = true;
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("CGPA: " + cgpa);
+        Console.WriteLine("Grade: " + grade);
+        Console.WriteLine("Student: " + isStudent);
+    }
+}
