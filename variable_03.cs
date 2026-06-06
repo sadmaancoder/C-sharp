44f0314ce2c065797e419691fa724c58799722b9
MD SADMAN <sadmansaif522@gmail.com>
2026-06-06 08:44:25 +0600

    Create variable_03.cs

---
+++ variable_03.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "SADMAN SAYED";
+        string batch = "64C";
+        int age = 20;
+        double cgpa = 3.70;
+        char grade = 'A';
+        bool isStudent = true;
+        
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Batch: " + batch);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("CGPA: " + cgpa);
+        Console.WriteLine("Grade: " + grade);
+        Console.WriteLine("Student: " + isStudent);
+        
+    }
+}
