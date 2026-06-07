1fc6405f0c008b2d2f0538c6e06c3f279612e8c6
MD SADMAN <sadmansaif522@gmail.com>
2026-06-07 09:30:14 +0600

    Create string_array.cs

---
+++ string_array.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+        for (int i = 0; i < cars.Length; i++)
+        {
+            Console.WriteLine(cars[i]);
+        }
+    }
+}
