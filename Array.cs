e23901709f60f20c787048ee78433af9df8850b7
MD SADMAN <sadmansaif522@gmail.com>
2026-06-07 09:25:23 +0600

    Create Array.cs

---
+++ Array.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
